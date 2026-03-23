c1420f12faac2cd8101c71fc6cbf539f95fd4b35
oscarlezcano <oscar.lezcano@fiuni.edu.py>
2026-03-23 01:32:10 -0300

    feat : add "PaginationDto"

---
+++ Models/Responses/Application/PaginationDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Models.Responses.Application
+{
+    public class PaginationResponseDto(int currentPage, int pageSize, int totalElements)
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "PageSize must be greater than 0.")]
+        public int CurrentPage { get; } = currentPage;
+        [Range(1, int.MaxValue, ErrorMessage = "PageSize must be greater than 0.")]
+        public int PageSize { get; } = pageSize;
+        public int TotalElements { get; } = totalElements;
+        public int TotalPages { get; } = pageSize > 0 ? (int)Math.Ceiling(totalElements / (double)pageSize) : 0;
+    }
+}
