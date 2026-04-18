a950c1df7578aea077ddc54eddc63b99422bb3db
oscarlezcano <oscar.lezcano@fiuni.edu.py>
2026-04-17 23:57:13 -0300

    chore: remove unnecesary comments

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -5,20 +5,12 @@ namespace BackEnd.Models;
 
 public partial class Stock
 {
-    public int Id { get; set; }
-
-    //DEPRECATED
-    //public int WarehouseId { get; set; }
-
     public int LoteId { get; set; }
 
     public int ProductId { get; set; }
 
     public decimal Quantity { get; set; }
 
-    //public virtual Lote Lote { get; set; } = null!;
-
     public virtual Product Product { get; set; } = null!;
 
-    //public virtual Warehouse Warehouse { get; set; } = null!;
 }
