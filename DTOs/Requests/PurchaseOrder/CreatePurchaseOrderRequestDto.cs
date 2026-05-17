8a30b5b4568bf9364a1bba7b95e48f90833dcae0
Heber Aranda <heberelcrack2005@gmail.com>
2026-05-17 17:31:14 -0300

    Add purchase order DTOs and mapper

---
+++ DTOs/Requests/PurchaseOrder/CreatePurchaseOrderRequestDto.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.DTOs.Requests.PurchaseOrder;
+
+public class PurchaseOrderDetailRequestDto
+{
+    [Required]
+    public int ProductId { get; set; }
+
+    [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+    public decimal QuantityOrdered { get; set; }
+
+    public int? SupplierQuoteDetailId { get; set; }
+}
+
+public class CreatePurchaseOrderRequestDto
+{
+    [Required]
+    public int PurchaseRequestId { get; set; }
+
+    [Required]
+    public int StateId { get; set; }
+
+    public int? SupplierId { get; set; }
+
+    public List<PurchaseOrderDetailRequestDto> Details { get; set; } = [];
+}
+
+public class UpdatePurchaseOrderRequestDto
+{
+    [Required]
+    public int PurchaseRequestId { get; set; }
+
+    [Required]
+    public int StateId { get; set; }
+
+    public int? SupplierId { get; set; }
+
+    [MinLength(1)]
+    public List<PurchaseOrderDetailRequestDto> Details { get; set; } = [];
+}
---
+++ DTOs/Responses/PurchaseOrder/PurchaseOrderResponseDto.cs
@@ -0,0 +1,48 @@
+using BackEnd.Utils;
+
+namespace BackEnd.DTOs.Responses.PurchaseOrder;
+
+public class PurchaseOrderDetailResponseDto
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public string? ProductName { get; set; }
+    public decimal QuantityOrdered { get; set; }
+    public decimal QuantityReceived { get; set; }
+    public decimal Price { get; set; }
+    public decimal TaxRate { get; set; }
+    public int? SupplierQuoteDetailId { get; set; }
+    public int? SupplierQuoteId { get; set; }
+    public int? SupplierId { get; set; }
+    public string? SupplierName { get; set; }
+}
+
+public class PurchaseOrderResponseDto
+{
+    public int Id { get; set; }
+    public int PurchaseRequestId { get; set; }
+    public int SupplierId { get; set; }
+    public string? SupplierName { get; set; }
+    public int? SupplierQuoteId { get; set; }
+    public string Number { get; set; } = null!;
+    public DateTime Date { get; set; }
+    public decimal Total { get; set; }
+    public int StateId { get; set; }
+    public List<PurchaseOrderDetailResponseDto> Details { get; set; } = [];
+}
+
+public class PurchaseOrderWrapperDto
+{
+    public PurchaseOrderResponseDto PurchaseOrder { get; set; } = null!;
+}
+
+public class PurchaseOrderDraftWrapperDto
+{
+    public PurchaseOrderResponseDto PurchaseOrder { get; set; } = null!;
+}
+
+public class ListPurchaseOrdersWrapperDto
+{
+    public List<PurchaseOrderResponseDto> PurchaseOrders { get; set; } = [];
+    public Pagination? Pagination { get; set; }
+}
