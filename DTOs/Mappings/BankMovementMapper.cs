95edb290155fdf73425cf8a295c08912bd84887a
geroxima <geroshima123@gmail.com>
2026-05-25 17:16:32 -0300

    fix: mapper bankmovement id

--- DTOs/Mappings/BankMovementMapper.cs
+++ DTOs/Mappings/BankMovementMapper.cs
@@ -1,5 +1,5 @@
 using AutoMapper;
-using BackEnd.Models;   
+using BackEnd.Models;
 using BackEnd.DTOs.Requests.Bank.BankMovement;
 using BackEnd.DTOs.Responses.Bank.BankMovement;
 
@@ -7,6 +7,8 @@ public class BankMovementMapper : Profile
 {
     public BankMovementMapper()
     {
+        CreateMap<BankMovement, BankMovementResponseDto>()
+            .ForMember(dest => dest.BankAccountId, opt => opt.MapFrom(src => src.AccountId));
         CreateMap<BankMovement, BankMovementRequestDto>();
         CreateMap<BankMovementRequestDto, BankMovement>();
         CreateMap<BankMovement, BankMovementResponseDto>();
