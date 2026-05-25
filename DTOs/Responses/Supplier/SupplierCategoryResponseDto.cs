a4210016047504e8232fcb4f6bc6c170983fc0ca
oscarlezcano <oscar.lezcano@fiuni.edu.py>
2026-05-25 17:40:47 -0300

    fix: SupplierCategory

--- DTOs/Responses/Supplier/SupplierCategoryResponseDto.cs
+++ DTOs/Responses/Supplier/SupplierCategoryResponseDto.cs
@@ -1,10 +1,23 @@
 namespace BackEnd.DTOs.Responses.Supplier;
+
 using BackEnd.Utils;
 using BackEnd.DTOs.Responses.ProductCategory;
-public class SupplierCategoryResponseDto        
+public class SupplierCategoryResponseDto
 {
     public int Id { get; set; }
     public int SupplierId { get; set; }
     public int ProductCategoryId { get; set; }
     public ProductCategoryResponseDto ProductCategory { get; set; } = null!;
-}    
+}
+
+public class SupplierCategoryWrapperDto
+{
+    public SupplierCategoryResponseDto SupplierCategory { get; set; } = null!;
+}
+
+// Crear todo los ListWrapper con paginacion
+public class ListSupplierCategoryWrapperDto
+{
+    public List<SupplierCategoryResponseDto> SupplierCategories { get; set; } = [];
+    public Pagination Pagination { get; set; } = null!;
+}
