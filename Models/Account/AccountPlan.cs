4bd0a886a4d85728fe743b9d8f6f11c6ac70976e
Joshua005 <patogallina1987@gmail.com>
2026-05-29 18:13:08 -0300

    feat: add AccountPlan and AccountantProcess models

---
+++ Models/Account/AccountPlan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackEnd.Models;
+
+public partial class AccountPlan
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public bool IsAcceptor { get; set; }
+
+    public int Level { get; set; }
+
+    public virtual ICollection<EntryDetail> EntryDetails { get; set; } = new List<EntryDetail>();
+
+    public virtual ICollection<EntryModelDetail> EntryModelDetails { get; set; } = new List<EntryModelDetail>();
+}
---
+++ Models/Account/AccountantProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackEnd.Models;
+
+public partial class AccountantProcess
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public int StateId { get; set; }
+
+    public virtual ICollection<Entry> Entries { get; set; } = new List<Entry>();
+
+    public virtual State State { get; set; } = null!;
+}
