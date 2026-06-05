386690e1a9da9d413e341d81f4ad529a11f4f518
Joshua005 <patogallina1987@gmail.com>
2026-06-04 22:31:01 -0300

    feat: add optional Description property to bank movement DTOs and models

--- Infrastructure/Context/AppDbContext.cs
+++ Infrastructure/Context/AppDbContext.cs
@@ -262,6 +262,7 @@ public partial class AppDbContext : DbContext
                 .HasColumnType("timestamp without time zone");
             entity.Property(e => e.MovementType).HasColumnType("bank_movement_type_enum");
             entity.Property(e => e.ReferenceNumber).HasMaxLength(100);
+            entity.Property(e => e.Description).HasMaxLength(500);
 
             entity.HasOne(d => d.Account).WithMany(p => p.BankMovements)
                 .HasForeignKey(d => d.AccountId)
